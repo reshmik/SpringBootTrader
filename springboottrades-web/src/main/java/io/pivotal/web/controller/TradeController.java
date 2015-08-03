2c39283bfed75042eb72dc08031966bd7de7bbbd
azwickey-pivotal <azwickey@gopivotal.com>
2015-08-02 20:26:01 -0400

    fixed error in account lookup reference

--- springboottrades-web/src/main/java/io/pivotal/web/controller/TradeController.java
+++ springboottrades-web/src/main/java/io/pivotal/web/controller/TradeController.java
@@ -17,15 +17,15 @@ import io.pivotal.web.service.MarketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -37,7 +37,17 @@ public class TradeController {
 	
 	@Autowired
 	private MarketService marketService;
-	
+
+	@RequestMapping(value = "/quote/{symbol}", method = RequestMethod.GET)
+	public ResponseEntity<Quote> quote(@PathVariable("symbol") final String symbol)  {
+		logger.debug("/trade.QUOTE");
+		Quote quote = marketService.getQuote(symbol);
+		logger.info(String.format("Retrieved symbol: %s with quote %s", symbol, quote));
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Cache-Control", "no-cache");
+		return new ResponseEntity<Quote>(quote, responseHeaders, HttpStatus.OK);
+	}
+
 	@RequestMapping(value = "/trade", method = RequestMethod.GET)
 	public String showTrade(Model model) {
 		logger.debug("/trade.GET");
